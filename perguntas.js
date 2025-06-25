ef2028dabda55233493892c703cb8ef97d9ec607
nathaeduardolimadoprado <prado.natha@escola.pr.gov.br>
2025-06-25 15:48:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'geografia',
+    'Qual é o maior país do mundo em extensão territorial?',
+    ' Rússia, com uma área de cerca de 17,1 milhões'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o rio mais longo do mundo?',
+    'O Rio Amazonas'
+)
+
+criaCartao(
+    'geografia',
+    'Qual continente é conhecido como "o berço da humanidade"?',
+    'A África'
+)
+
+criaCartao(
+    'geografia',
+    'Qual é o maior deserto do mundo?',
+    'O Deserto da Antártida'
+)
+
+criaCartao(
+    'geografia',
+    'Qual é o maior oceano do mundo?',
+    'O Oceano Pacífico'
+)
+
+criaCartao(
+    'geografia',
+    'Em que continente está localizado o Monte Everest?',
+    'Na Ásia, na cordilheira do Himalaia'
+)
